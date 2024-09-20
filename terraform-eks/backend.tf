53b542ac5bf8c562be5228e0dd4d6c7d48227530
Muhammad Rashid <rashiddaha80@gmail.com>
2024-09-20 05:52:23 +0500

    deploy

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "codewithmuh-test-12" # Replace with your actual S3 bucket name
+    bucket = "codewithmuh-test-13" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-west-1"
   }
